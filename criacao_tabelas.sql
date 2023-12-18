55672e3e14057d3f7775d6d07aa69f42fda0d27e
Camila Lima <camilam.oliveiralima@gmail.com>
2023-12-18 17:57:07 -0300

    Atualização banco de dados

--- criacao_tabelas.sql
+++ criacao_tabelas.sql
@@ -2,79 +2,62 @@
 CREATE DATABASE tatuagem_db
 
 -- Criação da tabela com informações dos clientes
-CREATE TABLE tatuagem_db.public.Cliente(
-  ClienteID SERIAL PRIMARY KEY,
-  CPF char(11) not null,
-  NomeCliente VARCHAR(40) NOT NULL,
-  Telefone VARCHAR(20),
-  Endereco VARCHAR(50),
-  Cidade VARCHAR (30),
-  Estado VARCHAR(2),
-  Pais VARCHAR(30),
-  CEP VARCHAR (9),
-  constraint unique_cpf_client unique(CPF)
+CREATE TABLE tatuagem_db.public.clientes(
+  cliente_id: INT SERIAL PRIMARY KEY,
+  cpf: varchar(11),
+  nome_cliente: VARCHAR(100),
+  telefone: VARCHAR(15),
+  endereco: VARCHAR(150),
+  cidade: VARCHAR (100),
+  estado: VARCHAR(50),
+  pais: VARCHAR(30),
+  cep: VARCHAR (9),
+  constraint unique_cpf_client unique(cpf)
 );
 
 -- Querie para vizualizar os dados da tabela cliente
-SELECT * FROM tatuagem_db.public.cliente;
-
--- Criação da tabela com informações dos fornecedores
-CREATE TABLE tatuagem_db.public.Fornecedor(
-  FornecedorID SERIAL PRIMARY KEY,
-  CNPJ CHAR(14) NOT NULL,
-  Telefone VARCHAR(20),
-  Endereco VARCHAR(50),
-  Cidade VARCHAR (30),
-  Estado VARCHAR(2),
-  Pais VARCHAR(30),
-  CEP VARCHAR (9),
-  CONSTRAINT unique_cnpj_fornecedor UNIQUE (CNPJ)
-);
-
--- Querie para vizualizar os dados da tabela fornecedor
-SELECT * FROM tatuagem_db.public.fornecedor;
+SELECT * FROM tatuagem_db.public.clientes;
 
 -- Criação da tabela com informações dos produtos
-CREATE TABLE tatuagem_db.public.Produto(
-  NumeroProduto SERIAL PRIMARY KEY,
-  NomeProduto VARCHAR(80) NOT NULL,
-  DescricaoProduto VARCHAR(180) NOT NULL,
-  CategoriaProduto VARCHAR (30),
-  AvaliacaoProduto VARCHAR(100),
-  Preco DECIMAL(10, 2) NOT NULL,
-  QuantidadeEstoque SMALLINT,
-  FOREIGN KEY (FornecedorID) REFERENCES Fornecedor(FornecedorID)
+CREATE TABLE tatuagem_db.public.produtos(
+  produto_id: int SERIAL PRIMARY KEY,
+  nome_produto: VARCHAR(100),
+  descricao_produto: text,
+  categoria_produto: VARCHAR (50),
+  avaliacao_produto: VARCHAR(100),
+  preco: float,
+  quantidade_estoque: INT,
 );
 
 -- Querie para vizualizar os dados da tabela produto
-SELECT * FROM tatuagem_db.public.produto;
+SELECT * FROM tatuagem_db.public.produtos;
 
 -- Criação da tabela com informações do pedido
-CREATE TABLE tatuagem_db.public.Pedido(
-  NumeroPedido SERIAL PRIMARY KEY,
-  StatusPedido varchar(30) NOT NULL,
-  DataPedido DATE,
-  DataRecebimento DATE,
-  PedidoTotal VARCHAR (30),
-  FOREIGN KEY (ClienteID) REFERENCES tatuagem_db.public.Cliente(ClienteID)
-  FOREIGN KEY (FuncionarioID) REFERENCES FuncionarioID(FuncionarioID)
+CREATE TABLE tatuagem_db.public.pedidos(
+  pedido_id : int SERIAL PRIMARY KEY,
+  status_pedido: varchar(50),
+  data_pedido: DATETIME,
+  data_recebimento: DATETIME,
+  pedido_total: float,
+  FOREIGN KEY (cliente_id) REFERENCES tatuagem_db.public.Clientes(cliente_id)
+  FOREIGN KEY (funcionario_id) REFERENCES funcionario_id(funcionario_id)
 );
 
 -- Querie para vizualizar os dados da tabela de pedido
-SELECT * FROM tatuagem_db.public.vendedor;
+SELECT * FROM tatuagem_db.public.pedidos;
 
 -- Criação da tabela com informações do funcionário
-CREATE TABLE tatuagem_db.public.Funcionario(
-  FuncionarioID SERIAL PRIMARY KEY,
-  NomeVendedor VARCHAR(80) NOT NULL,
-  Telefone VARCHAR(20),
-  Endereco VARCHAR(50),
-  Cidade VARCHAR (30),
-  Estado VARCHAR(2),
+CREATE TABLE tatuagem_db.public.funcionarios(
+  funcionario_id: int SERIAL PRIMARY KEY,
+  nome_vendedor VARCHAR(100),
+  Telefone VARCHAR(15),
+  Endereco VARCHAR(150),
+  Cidade VARCHAR (100),
+  Estado VARCHAR(50),
   Pais VARCHAR(30),
   CEP VARCHAR (9),
-  constraint unique_id_funcionario unique(FuncionarioID)
+  constraint unique_id_funcionario unique(funcionario_id)
 );
 
 -- Querie para vizualizar os dados da tabela Funcionario
-SELECT * FROM tatuagem_db.public.Funcionario;
+SELECT * FROM tatuagem_db.public.funcionarios;
--- dados.sql
+++ dados.sql
@@ -4,7 +4,7 @@ CREATE SEQUENCE inclusao_cliente
   increment 1;
 
 -- Inserção de dados na tabela cliente
-INSERT INTO Cliente (ClienteID, CPF, NomeCliente, Telefone, Endereco, Cidade, Estado, Pais, CEP)
+INSERT INTO clientes (cliente_id, cpf, nome_cliente, telefone, endereco, cidade, estado, pais, cep)
 VALUES  (nextval('inclusao_cliente'), '12345678901', 'Ana Paula', '11999998888', 'Rua Lima, 100 - Centro', 'São Paulo', 'SP', 'Brasil', '01234567'),
         (nextval('inclusao_cliente'), '23485776012', 'Pedro Luan', '11999997777', 'Avenida Paulista, 200 - Jardim Paulista', 'São Paulo', 'SP', 'Brasil', '01273000'),
         (nextval('inclusao_cliente'), '34567899993', 'Julia Sousa', '21999998888', 'Rua da Esperança, 1500 - Botafogo', 'Rio de Janeiro', 'RJ', 'Brasil', '45678901'),
@@ -12,39 +12,46 @@ VALUES  (nextval('inclusao_cliente'), '12345678901', 'Ana Paula', '11999998888',
         (nextval('inclusao_cliente'), '56789012345', 'Larissa Lima', '21999996666', 'Rua do Catete, 300 - Botafogo', 'Rio de Janeiro', 'RJ', 'Brasil', '49999901'),
         (nextval('inclusao_cliente'), '67890123456', 'Lucas Theo',  '11999996666','Rua Maranhão, 900', 'São Caetano do Sul', 'SP', 'Brasil', '01302000'),
 
--- Criação do ID do fornecedor serial iniciando em 1
-CREATE SEQUENCE inclusao_fornecedor
-  start 1
-  increment 1;
-
--- Inserção de dados na tabela fornecedor
-  FornecedorID SERIAL PRIMARY KEY,
-  CNPJ CHAR(14) NOT NULL,
-  Telefone VARCHAR(20),
-  Endereco VARCHAR(50),
-  Cidade VARCHAR (30),
-  Estado VARCHAR(2),
-  Pais VARCHAR(30),
-  CEP VARCHAR (9),
-
-INSERT INTO Fornecedor (FornecedorID, CNPJ, Telefone, Endereco, Cidade, Estado, Pais, CEP)
-VALUES	(nextval('inclusao_fornecedor'), '12345678701234', '(31) 2585-5555', 'Rua das Eras, 123 - Centro', 'Uberlandia', 'MG', 'Brasil', '33444555'),
-        (nextval('inclusao_fornecedor'), '23456789888099', '2236666666', 'Avenida das Tasca, 456 - Botafogo', 'Rio de Janeiro', 'RJ', 'Brasil', '45678901'),
-        (nextval('inclusao_fornecedor'), '34567890123456', '3332197777', 'Rua das Tulipas', 987, 'Belo Horizonte', 'MG', 'Brasil', '33666999'),
-        (nextval('inclusao_fornecedor'), '12345888550394', '1132345678', 'Rua das Cantareiras, 123 - Centro', 'São Paulo', 'SP', 'Brasil', '11222333'),
-        (nextval('inclusao_fornecedor'), '23456018345788', '223456-7890', 'Avenida das Ervas, 456 - Centro', 'Rio de Janeiro', 'RJ', 'Brasil', '41118901'),
-
 -- Criação do ID do produto serial iniciando em 1
 CREATE SEQUENCE inclusao_produto
   start 1
   increment 1;
 
 -- Inserção de dados na tabela produto
-INSERT INTO Produto (ProdutoID, Nome_produto, Fornecedorid, Categoria, Cor, Tamanho, Preco, Quantidade_estoque)
-VALUES	(nextval('inc_produto'), 'Camiseta básica', 1, 'Roupas', 'Verde',  'PP', 25.00, 100),
-       
-
-
+INSERT INTO Produtos (produto_id, nome_produto, descricao_produto, categoria_produto, avaliacao_produto, preco, quantidade_estoque)
+VALUES	(nextval('inclusao_produto'), 'Biqueira Electric Ink 32mm - Universal Fechada - Unidade', 'Ponteira', 'Bom', 4.10, 100),
+        (nextval('inclusao_produto'), 'Biqueira Electric Ink 32mm Crystal - Traço/Bucha - Unidade', 'Ponteira', 'Ótimo', 4.10, 100),
+        (nextval('inclusao_produto'), 'Biqueira Electric Ink 32mm - Pintura Magnum Fechado - Unidade', 'Ponteira', 'Bom', 4.10, 100),
+        (nextval('inclusao_produto'), 'Batoque Grande Com Base - 50 Unidades', 'Ponteira', 'Bom', 9.00, 100),
+        (nextval('inclusao_produto'), 'Batoque Médio Com Base - 50 Unidades', 'Ponteira', 'Ótimo', 4.00, 100),
+        (nextval('inclusao_produto'), 'Batoque Pequeno Com Base - 50 Unidades', 'Ponteira', 'Bom', 3.00, 100),
+        (nextval('inclusao_produto'), 'Biqueira Electric Ink 32mm - Pintura Magnum Aberto - Unidade', 'Ponteira', 'Bom', 4.10, 100),
+        (nextval('inclusao_produto'), 'Tinta Intenze - Lemon Yellow 30ml - Unidade', 'Tinta', 'Bom', 95.00, 100),
+        (nextval('inclusao_produto'), 'Tinta Intenze - Periwinkle 30ml- Unidade', 'Tinta', 'Bom', 95.00, 100),
+        (nextval('inclusao_produto'), 'Tinta Intenze - Bright Red 30ml - Unidade', 'Tinta', 'Bom', 95.00, 100),
+        (nextval('inclusao_produto'), 'Tinta Intenze - Platinum 30ml - Unidade', 'Tinta', 'Bom', 95.00, 100),
+        (nextval('inclusao_produto'), 'Agulha Aston Fácil Encaixe - 1RL - Unidade', 'Agulha', 'Regular', 13.00, 100),
+        (nextval('inclusao_produto'), 'Agulha Aston Fácil Encaixe Com Mola - 3RL- Unidade', 'Agulha', 'Regular', 15.00, 100),
+        (nextval('inclusao_produto'), 'Agulha Easy Balance - 1 Ponta XLT - DermoMag - Unidade', 'Agulha', 'Regular', 22.00, 100),
+        (nextval('inclusao_produto'), 'Luva Nitrílica Descartável Sem Pó Black - Unigloves Premium - Caixa Com 100 Unidades', 'Luva', 'Bom', 55.00, 100),
+        (nextval('inclusao_produto'), 'Luva Látex Descartável Com Pó Pink - Unigloves Classic - Caixa Com 100 Unidades', 'Luva', 'Bom', 55.00, 100),
+        (nextval('inclusao_produto'), 'Luva Látex Descartável Sem Pó Green Lano-E - Unigloves Premium - Caixa Com 100 Unidades', 'Luva', 'Bom', 40.00, 100),
+        (nextval('inclusao_produto'), 'No Plastic MBOAH - 5g- Unidade', 'Plástico', 'Bom', 6.24, 100),
+        (nextval('inclusao_produto'), 'Plástico Filme PVC - 28cm X 30m - Unidade', 'Plástico', 'Bom', 6.24, 100),
+        (nextval('inclusao_produto'), 'Plástico Filme PVC Com Cabo - 12cm X 140m Unidade', 'Plástico', 'Bom', 26.55, 100),
+        (nextval('inclusao_produto'), 'Plástico Filme PVC - 28cm X 300m- Unidade', 'Plástico', 'Bom', 44.00, 100),
+        (nextval('inclusao_produto'), 'Coconut Vaseline MBOAH - 440g - Unidade', 'Vaselina', 'Bom', 59.10, 100),
+        (nextval('inclusao_produto'), 'Vaselina Vasipro Artpig - 420g- Unidade', 'Vaselina', 'Bom', 61.11, 100),
+        (nextval('inclusao_produto'), 'Vaselina Easy Inn Butter Blend - 300g- Unidade', 'Vaselina', 'Regular', 99.00, 100),
+        (nextval('inclusao_produto'), 'Máquina Rotativa Para Tatuagem X13 - Silver- Unidade', 'Maquina', 'Bom', 260.00, 100),
+        (nextval('inclusao_produto'), 'Máquina Rotativa Para Tatuagem X13 - Blue- Unidade', 'Maquina', 'Bom', 260.00, 100),
+        (nextval('inclusao_produto'), 'Máquina Rotativa Taurus Rosa - Trestini Unidade', 'Maquina', 'Bom', 280.00, 100),
+        (nextval('inclusao_produto'), 'Máquina Rotativa Para Tatuagem X13 Pop - Blue Unidade', 'Maquina', 'Bom', 367,37, 100),
+        (nextval('inclusao_produto'), 'Cartucho DK Lab - Traço RL - Unidade', 'Cartucho', 'Regular', 8.35, 100),
+        (nextval('inclusao_produto'), 'Cartucho Arkham - Traço RL - Unidade', 'Cartucho', 'Regular', 8.35, 100),
+        (nextval('inclusao_produto'), 'Cartucho Arkham - Bucha RS - Unidade', 'Cartucho', 'Regular', 10.50, 100),
+        (nextval('inclusao_produto'), 'Cartucho Hornet - Bucha 11RS - Unidade', 'Cartucho', 'Regular', 13.40, 100),
+        (nextval('inclusao_produto'), 'Corpo Grip Em Aço Para Ponteira - 16MM - Unidade', 'Grip', 'Regular', 28.00, 100),
 
 -- Criação do ID do funcionario serial iniciando em 1
 CREATE SEQUENCE inclusao_funcionario
@@ -52,7 +59,7 @@ CREATE SEQUENCE inclusao_funcionario
   increment 1;
 
 -- Inserção de dados na tabela Funcionario
-INSERT INTO Funcionario (FuncionarioID, NomeVendedor, Telefone, Endereco, Cidade, Estado, Pais, CEP)
+INSERT INTO funcionarios (funcionario_id, nome_vendedor, telefone, endereco, cidade, estado, pais, cep)
 VALUES	(nextval('inclusao_funcionario'), 'Paulo Fernandes', '11900008888', 'Rua Eras, 321 - Centro', 'São Paulo', 'SP', 'Brasil', '03122200'),
         (nextval('inclusao_funcionario'), 'Paula Oliveira', '11900022777', 'Rua Genebra, 1321 - Centro', 'São Paulo', 'SP', 'Brasil', '01312200'),
         (nextval('inclusao_funcionario'), 'Rafaela Ferreira', '11900006666', 'Rua Baraldi, 321', 'São Caetano do Sul', 'SP', 'Brasil', '01132200'),
@@ -64,7 +71,7 @@ CREATE SEQUENCE inclusao_pedido
   increment 1;
 
 -- Inserção de dados na tabela pedido
-INSERT INTO Pedidos (NumeroPedido, StatusPedido, DataPedido, DataRecebimento, PedidoTotal, ClienteID, FuncionarioID)
+INSERT INTO pedidos (pedido_id, status_pedido, data_pedido, data_recebimento, pedido_total, cliente_id, funcionario_id)
 VALUES (nextval('inclusao_pedido'), 'Concluido', '2023-11-20', '2023-11-25', '250,00', 3, 1),
        (nextval('inclusao_pedido'), 'Concluido', '2023-11-10', '2023-11-25', '180,00', 2, 1),
        (nextval('inclusao_pedido'), 'Em Separação', '2023-10-20', '2023-11-05', '50,00', 1, 1),
